f314aac3a010be3ed7054d41e86b61281443c8c0
JaviGarcia <celmerjavier72@hotmail.com>
2023-11-10 09:38:57 -0600

    updating code

--- src/index.js
+++ src/index.js
@@ -19,7 +19,7 @@ const app = express();
 
 app.get('/', (req, res) => {
   const name = process.env.NAME || 'World';
-  res.send(`Hello ${name}!`);
+  res.send(`Hola ${name}!`);
 });
 
 const port = process.env.PORT || 8090;
